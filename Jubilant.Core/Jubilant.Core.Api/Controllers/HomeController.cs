4873d17589dfd134f3af4a2d440d664e60d52125
Khondamir Abdukhoshimov <iCloudBMX0525@gmail.com>
2022-03-15 23:43:49 +0500

    INFRA: Initialize Core API Project

---
+++ Jubilant.Core/Jubilant.Core.Api/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace Jubilant.Core.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : RESTFulController
+    {
+        [HttpGet]
+        public ActionResult<string> Get() =>
+            Ok("Hello Mario, the princess is in another catle.");
+    }
+}
